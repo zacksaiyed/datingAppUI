0be3625d6c30d03af23258c78e898014603f7164
Zaid Saiyed <zaid.saiyed@technovate.in>
2021-10-01 22:37:18 +0530

    first commit ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'datingAppUI';
+  users :any;
+
+  constructor( private http:HttpClient){}
+
+  ngOnInit(){
+   this.getUsers();
+  }
+
+  getUsers(){
+    this.http.post("https://localhost:44316/api/Users/GetUsers",null).subscribe(res=>{
+      this.users=res;
+    },error=>{
+      console.log(error);
+    })
+  }
 }
